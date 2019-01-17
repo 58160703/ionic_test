903dd0f8fb7c3efff087c43b05f28152d5f6eb73
Warintorn <58160703@go.buu.ac.th>
2019-01-17 16:37:33 +0700

    pluse UI silde

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ContactPage } from '../contact/contact';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -12,6 +13,26 @@ export class HomePage {
   company: string = 'A-Host Company';
   Products: string = 'A';
   showing: boolean = false;
+
+  slides = [
+    {
+      title: "Welcome to the Docs!",
+      description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
+      image: "../../assets/imgs/ica-slidebox-img-1.png",
+      
+    },
+    {
+      title: "What is Ionic?",
+      description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+      image: "../../assets/imgs/ica-slidebox-img-2.png",
+    },
+    {
+      title: "What is Ionic Cloud?",
+      description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+      image: "../../assets/imgs/ica-slidebox-img-3.png",
+    }
+  ];
+
     
   constructor(public navCtrl: NavController) { 
     
